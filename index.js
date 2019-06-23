8023ebfec16ef8e67382941374a2afbec967211b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-23 18:44:48 -0400

    Completed lab

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = function(array) {
+  const obj = array.find(ele => ele.result === 'W');
+
+  if (typeof obj === 'undefined') {
+    return undefined 
+  } else {
+    return obj.year
+  }
+
+}
