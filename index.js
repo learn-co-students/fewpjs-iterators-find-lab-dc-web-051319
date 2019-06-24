87a95e84200c98e2735703f93eb35ce3cdb2d048
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-24 01:26:40 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(recordHash)
+{
+  
+  if (recordHash.find(e => e.result === "W") === undefined) 
+  {
+    return undefined 
+  }
+  
+  let champsHash = recordHash.find(e => e.result === "W");
+
+  return champsHash.year  
+  
+}
