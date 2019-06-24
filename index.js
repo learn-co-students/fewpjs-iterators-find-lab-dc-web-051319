a9c47e5f41359aa32ae4a6f1fe45f1c367862785
Emily Bolten <emilybolten@gmail.com>
2019-06-24 16:19:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result = array.find(array => array.result === 'W');
+  if (!!result) {
+    return result.year
+  };
+};
