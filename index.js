c4bbfd8f55e7ed7c505a4874a9ef338f97e61668
Shujaat Azim <shujaat.azim@gmail.com>
2019-06-27 22:34:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
-function testFunc() {
-  return "hi"
+// THIS WORKS BUT WITHOUT FIND waaaah
+
+// function superbowlWin(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].result === "W") {
+//       return arr[i].year;
+//     }
+//   }
+// } 
+
+
+function superbowlWin(arr) {
+  let result = arr.find( (obj) => {
+    return obj.result === "W";
+  })
+  if (result) {
+    return result.year;
+  }
 }
